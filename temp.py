ce8feb7621ec376ea6072cb5bedb5dd5d0cf53c7
Andrew Szeto <andrew@jabagawee.com>
2013-09-04 15:59:53 -0700

    Initial commit

---
+++ temp.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+import re
+import sys
+
+r = re.compile(r"^(aa+)\1+$")
+
+def factor(n):
+    match = r.match("a" * n)
+    if not match:
+        return [n]
+    f = len(match.groups()[0])
+    return factor(f) + factor(f/n)
+
+if __name__ == "__main__":
+    print factor(int(sys.argv[1]))
