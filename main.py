45bbce7274df98bef6fc41646fbdfa547913cd04
Andrew Szeto <andrew@jabagawee.com>
2013-09-05 18:00:14 -0700

    Optimization, aww yeah

--- main.py
+++ main.py
@@ -3,14 +3,14 @@
 import re
 import sys
 
-r = re.compile(r"^(aa+)\1+$")
+r = re.compile(r"^(aa+?)\1+$")
 
 def factor(n):
     match = r.match("a" * n)
     if not match:
         return [n]
     f = len(match.groups()[0])
-    return factor(n/f) + factor(f)
+    return factor(f) + factor(n/f)
 
 if __name__ == "__main__":
     print factor(int(sys.argv[1]))
